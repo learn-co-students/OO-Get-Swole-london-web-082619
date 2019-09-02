abf1f9747a1edce49dbe0af97f92691f3bd25014
github username <github email address>
2019-09-02 13:24:08 +0100

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,43 @@
 class Gym
-  attr_reader :name
+  attr_reader :name, :cost
 
-  def initialize(name)
+  @@all = []
+
+  def initialize(name, cost)
     @name = name
+    @cost = cost
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def cost
+    @cost
+  end
+
+  def memberships
+    #Get a list of all memberships at a specific gym
+    Membership.all.select{|mem_ins| mem_ins.gym == self}
+  end
+
+
+  def members
+    #Get a list of all the lifters that have a membership to a specific g
+    memberships.map {|m| m.member}
+  end
+
+  def member_names
+    #Get a list of the names of all lifters that have a membership to that gym
+    memberships.map {|m| m.member.name}
+  end
+
+  def gym_lift_total
+    #Get the combined lift total of every lifter has a membership to that gym
+    combined_total = 0
+    members.each {|m| combined_total += m.lift_total}
+    combined_total
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,31 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+ff = Gym.new("Fitness First", 100)
+pg = Gym.new("PureGym", 30)
+hp = Gym.new("Holmes Place", 60)
+
+=begin
+lifter = "Yves, Bapo, Caspar, Nayden, Louis, Emin, Lukas".split(", ")
+lifter.each {|l| Lifter.new(l, (10..100).to_a.sample)}
+Lifter.all.each {|l| l = l.name.to_s.downcase}
+=end
+
+yves = Lifter.new("Yves", 90)
+nayden = Lifter.new("Nayden", 75)
+bapo = Lifter.new("Bapo", 80)
+caspar = Lifter.new("Caspar", 95)
+emin = Lifter.new("Emin", 75)
+louis = Lifter.new("Louis", 85)
+ 
+yves.make_membership(ff)
+yves.make_membership(pg)
+louis.make_membership(hp)
+louis.make_membership(ff)
+louis.make_membership(pg)
+bapo.make_membership(pg)
+caspar.make_membership(pg)
+emin.make_membership(ff)
 
 binding.pry
 
