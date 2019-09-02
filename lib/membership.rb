6d4b9f3249b26a9b3399585ee658720ae41b2687
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-02 11:14:30 +0100

    Done.

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,18 @@
 class Membership
   attr_reader :cost
+  attr_accessor :lifter, :gym
+  @@all = []
 
-  def initialize(cost)
+  def initialize(lifter, gym, cost) #Membership.new(lifter, gym, cost)
+    @lifter = lifter
+    @gym = gym
     @cost = cost
+    @@all << self
   end
+
+  #Get a list of all memberships
+  def self.all
+    @@all
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,21 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+#initialize sample lifters
+sam = Lifter.new("Sam", 125)
+john = Lifter.new("John", 200)
+lisa = Lifter.new("Lisa", 300)
+tom = Lifter.new("Tom", 50)
+rachel = Lifter.new("Rachel", 30)
+
+#initialize sample gyms
+rebel = Gym.new("1Rebel")
+mos = Gym.new("Ministry of Sound Fitness")
+un1t = Gym.new("UN1T")
+
+#give each lifter a membership
+Lifter.all.each { |lifter| Membership.new(lifter, Gym.all.sample, rand(25..200)) }
+
 binding.pry
 
 puts "Gains!"
