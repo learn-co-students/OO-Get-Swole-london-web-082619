26330f62033b6c0fe3f78797c1411d2d45cb7566
github username <github email address>
2019-09-04 00:06:15 +0100

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,22 @@ require 'pry'
 require_relative '../lib/gym'
 require_relative '../lib/lifter'
 require_relative '../lib/membership'
+
+lifter1 = Lifter.new("Chuck", 50)
+lifter2 = Lifter.new("Dom", 100)
+lifter3 = Lifter.new("Adam", 150)
+lifter4 = Lifter.new("Lewis", 200)
+
+gym1 = Gym.new("PureGym")
+gym2 = Gym.new("Big Gym")
+gym3 = Gym.new("THE Gym")
+gym4 = Gym.new("Swole Gym")
+
+m1 = Membership.new(lifter1, gym1, 200)
+m2 = Membership.new(lifter1, gym2, 150)
+m3 = Membership.new(lifter2, gym1, 175)
+m4 = Membership.new(lifter3, gym3, 250)
+
+
+binding.pry
+puts "Gains!"
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,35 @@
 class Lifter
-  attr_reader :name, :lift_total
+  attr_reader :name, :lift_total, :lifter
+@@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|memberships| memberships.lifter == self}
+  end
+
+  def gyms
+    self.memberships.map{|memberships| memberships.gym}
+  end
+
+  def self.average_lift
+    @@all.sum{|lifter| lifter.lift_total}/@@all.length
+  end
+
+  def total_cost
+    self.memberships.sum{|membership| membership.cost}
+  end
+
+  def sign_up(gym, cost)
+    Membership.new(self, gym, cost)
+  end
+
 end
